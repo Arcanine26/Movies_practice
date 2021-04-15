9a6a7b35d7edd253f62790cce5ac4337f2f66970
Vladislav <vlad.skraimer@gmail.com>
2021-04-15 12:27:22 +0300

    optimized(right) version

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-let personalMovieDB = {
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+const lastWatch1 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating1 = prompt('На сколько вы оцените его?'),
+      lastWatch2 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating2 = prompt('На сколько вы оцените его?');
+personalMovieDB.movies[lastWatch1] = rating1;
+personalMovieDB.movies[lastWatch2] = rating2;
 console.log(personalMovieDB);
-const lastWatch = prompt('Один из последних просмотренных фильмов?', '');
-const lastWatch2 = prompt('Один из последних просмотренных фильмов?', '')
-const rating = prompt('На сколько оцените первый фильм', '');
-const rating2 = prompt('На сколько оцените второй фильм', '');
-let movies = {
-    [lastWatch]: rating,
-    [lastWatch2]: rating2
-};
-console.log(movies);
